01d987b049fed5a6bfe7895a6f7b576b413ac777
Matthieu Robin <m.robin@meetic-corp.com>
2024-01-29 22:52:44 +0100

    WIP: add thumbnails

--- commands.js
+++ commands.js
@@ -1,10 +1,12 @@
 //import example from './commands/example.js';
 import albumDates from './commands/albumDates.js';
 import findOutOfAlbumPhotos from './commands/findOutOfAlbumPhotos.js';
+import findOutOfAlbumPhotosWithThumbnail from './commands/findOutOfAlbumPhotosWithThumbnail.js';
 
 export default [
 
 	albumDates,
-	findOutOfAlbumPhotos
+	findOutOfAlbumPhotos,
+	findOutOfAlbumPhotosWithThumbnail
 
 ].flat(); // Individual commands imports may return arrays of commands, flatten them here.
